22b5c3a3ca733814842a918924ec52c5e3d41505
eiichi sugiyama <barcarunrunbiscabarca@gmail.com>
2020-05-06 11:09:11 +0900

    mdb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test/test.module';
+import { UsersModule } from './users/users.module';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     TestModule,
-
-
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
