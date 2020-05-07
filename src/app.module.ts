8b716729813d8943eb6a1658bb364e0568bd2843
empelt <tetsuyakato2001@yahoo.co.jp>
2020-05-07 15:13:36 +0900

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,8 +12,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '34.80.234.243',
-      port: 3000,
+      host: '127.0.0.1',
+      port: 3306,
       username: 'root',
       password: 'techf0rward',
       database: 'new_schema',
