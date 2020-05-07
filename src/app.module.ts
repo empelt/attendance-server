7ad29bf46221ce18cb87ba52856d92bf8f27e262
eiichi sugiyama <barcarunrunbiscabarca@gmail.com>
2020-05-07 15:10:52 +0900

    sugi修正

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,26 +3,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test/test.module';
 import { UsersModule } from './users/users.module';
+import { Connection } from 'typeorm';
+
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     TestModule,
-
+    UsersModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '34.80.234.243',
-      port: 3000,
+      host: '127.0.0.1',
+      port: 3306,
       username: 'root',
       password: 'techf0rward',
       database: 'new_schema',
       entities: [],
-      synchronize: true,
+      // synchronize: true,
+      autoLoadEntities: true,
     }),
-
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
+
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,6 +6,7 @@ import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
+   exports: [TypeOrmModule],
   providers: [UsersService],
   controllers: [UsersController],
 })
