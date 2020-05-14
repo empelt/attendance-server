d44b2f7a10d6b8c423084275eff81c65f8558edf
empelt <tetsuyakato2001@yahoo.co.jp>
2020-05-14 23:24:31 +0900

    ad

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = Number(process.env.PORT) || 3000;
   const app = await NestFactory.create(AppModule, { cors: true });
-   app.enableCors({
-     origin: '*',
-     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-   });
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
   await app.listen(PORT);
 }
 bootstrap();
